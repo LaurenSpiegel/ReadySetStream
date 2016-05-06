105176630260c5578125a1843d98e62ae9061d7d
Lauren Spiegel <lhspiegel@gmail.com>
2016-05-05 18:23:32 -0700

    Keep track of writable based on size rather than listening for event.

--- MemDuplex.js
+++ MemDuplex.js
@@ -8,15 +8,17 @@ const Duplex = require('stream').Duplex;
  * @return {MemDuplex} a MemDuplex instance
  */
 class MemDuplex extends Duplex {
-    constructor() {
+    constructor(totalLength) {
         super();
         this.buffers = [];
         this.cur = 0;
-        this.finish = false;
+        this.totalLength = totalLength;
+        this.receivedLength = 0;
     }
     _write(chunk, enc, cb) {
         assert(Buffer.isBuffer(chunk));
         this.buffers.push(chunk);
+        this.receivedLength += Buffer.byteLength(chunk);
         cb();
     }
     _read() {
@@ -24,7 +26,8 @@ class MemDuplex extends Duplex {
             this.push(this.buffers[this.cur], 'binary');
             this.cur++;
         }
-        if (this.cur >= this.buffers.length && this.finish) {
+        if (this.cur >= this.buffers.length &&
+            this.receivedLength >= this.totalLength) {
             this.push(null); // End of buffer
         }
     }
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ exports.readySetStream = function readySetStream(locations, dataRetrievalFn,
         if(!logger){
             logger = console;
         }
-    // If shifted all locations out of array, stop
+  // If shifted all locations out of array, stop
     if (locations.length === 0) {
         return response.end();
     }
@@ -16,34 +16,26 @@ exports.readySetStream = function readySetStream(locations, dataRetrievalFn,
     const item = locations.shift();
     // Take the next item
     const nextItem = locations.shift();
-    const memDuplex = new MemDuplex();
-    let outerScopeNextReadable;
+    let memDuplex;
     if (nextItem) {
+        memDuplex = new MemDuplex(nextItem.size);
       // Get the next item from data and pipe
       // into the in-memory duplex
-        dataRetrievalFn(nextItem, logger, (err, nextReadable) => {
+        dataRetrievalFn(nextItem.key, logger, (err, nextReadable) => {
             if (err) {
                 logger.error('failed to get full object');
                 return response.socket.end();
             }
-            outerScopeNextReadable = nextReadable;
             nextReadable.pipe(memDuplex);
             nextReadable.on('error', () => {
                 logger.error('error piping data from source');
                 return response.socket.end();
             });
-            // Need to listen here for the end on streaming
-            // to the memDuplex in case the pipe to
-            // the memDuplex finishes before the pipe
-            // from the lead item to the response
-            nextReadable.on('end', () => {
-                memDuplex.finish = true;
-            });
         });
     }
     // Get the data, pipe the lead item to the response and keep
     // the response stream open
-    dataRetrievalFn(item, logger, (err, readable) => {
+    dataRetrievalFn(item.key, logger, (err, readable) => {
         if (err) {
             logger.error('failed to get full object');
             return response.socket.end();
@@ -55,51 +47,21 @@ exports.readySetStream = function readySetStream(locations, dataRetrievalFn,
         });
         readable.on('end', () => {
             if (nextItem) {
-              // Check whether the pipe from the nextItem
-              // to the memDuplex already finished
-                if (memDuplex.finish) {
-                    // Pipe from the memDuplex to the
-                    // response
-                    memDuplex.pipe(response,
-                        { end: false });
-                    memDuplex.on('error', () => {
-                        logger.error('error piping ' +
-                        'data from in memory duplex');
-                        return response.socket.end();
-                    });
-                    memDuplex.on('end', () => {
-                        // Move on to the next items in the
-                        // group (or get a new group if
-                        // group array is now empty due to
-                        // the shifts out)
-                        readySetStream(locations, dataRetrievalFn,
-                            response, logger);
-                    });
-                } else {
-                    // If piping from the lead item to the
-                    // response was faster than piping the
-                    // nextItem to the memDuplex, wait until
-                    // the nextItem has finished streaming to
-                    // the memDuplex
-                    outerScopeNextReadable.on('end', () => {
-                        memDuplex.finish = true;
-                        // Pipe from the memDuplex to the
-                        // response
-                        memDuplex.pipe(response,
-                          { end: false });
-                        memDuplex.on('error', () => {
-                            logger.error('error piping data ' +
-                            'from in memory duplex');
-                            return response.socket.end();
-                        });
-                        memDuplex.on('end', () => {
-                            // Move on to the next items
-                            // in the group
-                            readySetStream(locations, dataRetrievalFn,
-                                response, logger);
-                        });
-                    });
-                }
+                // Pipe from the memDuplex to the
+                // response
+                memDuplex.pipe(response,
+                    { end: false });
+                memDuplex.on('error', () => {
+                    logger.error('error piping ' +
+                    'data from in memory duplex');
+                    return response.socket.end();
+                });
+                memDuplex.on('end', () => {
+                    // Move on to the next items
+                    // (or finish if no more locations)
+                    process.nextTick(readySetStream, locations,
+                        dataRetrievalFn, response, logger);
+                });
             } else {
                 return readySetStream(locations, dataRetrievalFn,
                     response, logger);
@@ -107,3 +69,4 @@ exports.readySetStream = function readySetStream(locations, dataRetrievalFn,
         });
     });
 };
+
