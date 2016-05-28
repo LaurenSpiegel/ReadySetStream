a1553d1ed32c825ce5e12a5aa8112ef924026d19
Lauren Spiegel <lhspiegel@gmail.com>
2016-05-27 18:47:29 -0700

    Add unit tests.

--- tests/unit/readySetStream.js
+++ tests/unit/readySetStream.js
@@ -1,10 +1,145 @@
+'use strict';
+
 const assert = require('assert');
+const async = require('async');
+const crypto = require('crypto');
+const EventEmitter = require('events').EventEmitter;
+const fs = require('fs');
+const httpMocks = require('node-mocks-http');
+const proc = require('child_process');
 const readySetStream = require('../../index').readySetStream;
 
+const file10MBData = new Buffer(10485760).fill('Rahul');
+const file20MBData = new Buffer(20971520).fill('Marc');
+const file30MBData = new Buffer(31457280).fill('Rached');
+const file40MBData = new Buffer(41943040).fill('Giorgio');
 
-describe('testing', () => {
-    it('should test', () => {
-        const expectedOutput = 'hello';
-        assert.strictEqual('hello', expectedOutput);
+const files = {
+    file10MB: file10MBData,
+    file20MB: file20MBData,
+    file30MB: file30MBData,
+    file40MB: file40MBData,
+};
+
+function createFiles(fileNamesSizes, callback) {
+    return async.forEachOf(fileNamesSizes,
+        function createFile(data, name, next) {
+            return fs.writeFile(name, data, next);
+        },
+        err => {
+            assert.ifError(err);
+            return callback();
+        });
+}
+
+function deleteFiles(files, callback) {
+    proc.spawn('rm', files).on('exit', code => {
+        assert.strictEqual(code, 0);
+        return callback();
+    });
+}
+
+function dataRetrieval(key, logger, callback) {
+    const readStream = fs.createReadStream(key);
+    return callback(null, readStream);
+}
+
+function testBody(filesTested, callback) {
+    let expectedHash = crypto.createHash('md5');
+    filesTested.forEach(fileName => {
+        expectedHash.update(`${files[fileName]}`);
+    });
+    expectedHash = expectedHash.digest('hex');
+    const response = httpMocks.createResponse({
+        eventEmitter: EventEmitter,
+    });
+    response.on('end', () => {
+        const data = response._getData();
+        const finalHash = crypto.createHash('md5')
+        .update(data).digest('hex');
+        assert.strictEqual(finalHash, expectedHash);
+        return callback();
+    });
+    readySetStream(filesTested, dataRetrieval, response);
+}
+
+describe('readySetStream', () => {
+    before(done => {
+        createFiles(files, done);
+    });
+
+    after(done => {
+        deleteFiles(Object.keys(files), done);
+    });
+
+
+    it('should get data that is stored in one location', done => {
+        return testBody(['file10MB'], done);
+    });
+
+
+    it('should get data of two parts where each part is of equal ' +
+        'size', done => {
+        return testBody(['file10MB', 'file10MB'], done);
+    });
+
+
+    it('should get data of two parts where the first part is bigger ' +
+        'than the second part', done => {
+        return testBody(['file20MB', 'file10MB'], done);
+    });
+
+
+    it('should get data of two parts where the first part is much bigger ' +
+        'than the second part', done => {
+        return testBody(['file40MB', 'file10MB'], done);
+    });
+
+
+    it('should get data of two parts where the first part is smaller ' +
+        'than the second part', done => {
+        return testBody(['file10MB', 'file20MB'], done);
+    });
+
+
+    it('should get data of two parts where the first part is ' +
+        'much smaller than the second part', done => {
+        return testBody(['file10MB', 'file40MB'], done);
+    });
+
+
+    it('should get data of three parts where the size of each part is ' +
+        'bigger than the previous', done => {
+        return testBody(['file10MB', 'file20MB', 'file30MB'], done);
+    });
+
+
+    it('should get data of three parts where the size of each part is ' +
+        'smaller than the previous', done => {
+        return testBody(['file40MB', 'file30MB', 'file20MB'], done);
+    });
+
+
+    it('should get data of four parts where the size of each part is ' +
+        'bigger than the previous', done => {
+        return testBody(['file10MB', 'file20MB', 'file30MB', 'file40MB'], done);
+    });
+
+
+    it('should get data of four parts where the size of each part is ' +
+        'smaller than the previous', done => {
+        return testBody(['file40MB', 'file30MB', 'file20MB', 'file10MB'], done);
+    });
+
+
+    it('should get data of four parts with mixed part sizes', done => {
+        return testBody(['file20MB', 'file10MB', 'file30MB', 'file40MB'], done);
+    });
+
+
+    it('should get data of five parts with mixed part sizes', done => {
+        return testBody(['file20MB', 'file30MB', 'file40MB', 'file10MB',
+            'file30MB'], done);
     });
 });
+
