8b7e4ae01b0ef960ffa8ac528b4dc1a2f940f571
Lauren Spiegel <lhspiegel@gmail.com>
2017-05-26 10:14:20 -0700

    additional testing

--- MemDuplex.js
+++ MemDuplex.js
@@ -1,8 +1,8 @@
-
 'use strict';
 
 const assert = require ('assert');
 const Duplex = require('stream').Duplex;
+const Transform = require('stream').Transform;
 
 /**
  * Creates in memory writable and readable stream
@@ -12,9 +12,11 @@ class MemDuplex extends Duplex {
     constructor(location) {
         // Piping to the memDuplex will pause if writableBuffer
         // exceeds this amount
-        super({ highWaterMark: 8 * 1024 * 1024 });   // 8MB
+        // super({ highWaterMark: 8 * 1024 * 1024 });   // 8MB
+        super();
         this.buffers = [];
         this.location = location;
+        this.readyToReadMore = true;
         // Once writable stream is finished it will emit a finish
         // event
         this.once('finish', () => {
@@ -24,24 +26,41 @@ class MemDuplex extends Duplex {
     _write(chunk, enc, cb) {
         assert(Buffer.isBuffer(chunk));
         this.buffers.push(chunk);
-        this._read();
+        if (this._readableState.length <
+            this._readableState.highWaterMark
+            || this._readableState.needReadable) {
+            this._read(this._readableState.highWaterMark);
+            }
         cb();
     }
     _read() {
-        while (this.buffers.length > 0) {
-            const pushed = this.push(this.buffers.shift(), 'binary');
-            if (!pushed) {
-                break;
-            }
+        // console.log("this in read!!", this)
+        if (this.buffers.length > 0 && this._readableState.length < this._readableState.highWaterMark) {
+            let upAtBat = this.buffers.shift();
+            const pushed = this.push(upAtBat, 'binary');
+            upAtBat = null;
+            // if (!pushed) {
+            //     break;
+            // }
         }
+
         // finished is property of writable stream
         // indicating writing is done.
         // Once the writing is done and we have pushed
         // out everything buffered, we're done
         if (this.buffers.length === 0 && this._writableState.finished) {
+            // delete this.buffers;
+            console.log("this!!", this)
             this.push(null); // End of buffer
         }
     }
+
+
+
+    // _transform(chunk, enc, cb){
+    //     return cb(null, chunk);
+    // }
 }
 
 module.exports = MemDuplex;
+
